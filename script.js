38c8112b6476bbefa71d32586ac16f948d78c593
Matheus Rodrigues <mth.rodriguess@gmail.com>
2019-09-16 11:24:13 -0300

    criar estrutura e funções

---
+++ script.js
@@ -0,0 +1,21 @@
+let lista = document.querySelectorAll('h1');
+let caixa = document.querySelector('#caixa');
+let botao = document.querySelector('#botao');
+
+function verificarValor(letra){
+    for  (let h1 of lista){
+        if(letra == h1.innerHTML){
+            h1.style.color = 'white'
+        }else{
+            
+        }
+    }
+    
+}
+
+function pegarLetra(){
+    let letra = caixa.value;
+    verificarValor(letra)
+}
+
+botao.onclick = pegarLetra;
