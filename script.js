4e543d201d6ceb1802d1c052c805546841dce817
nallumartins <contatonathaliamartins@gmail.com>
2019-09-21 17:10:16 -0300

    atualizar logica js

--- script.js
+++ script.js
@@ -3,21 +3,26 @@ let caixa = document.querySelector('#caixa');
 let botao = document.querySelector('button');
 
 function verificarValor(letra){
+    let exist = false;
+
     for  (let h1 of lista){
         if(letra == h1.innerHTML){
-            h1.style.color = 'white'
-        }else{
-            
+            h1.style.color = 'white';
+            exist = true;
         }
     }
     
-}
+    if (!exist){
+        alert("erro");
+    }
+} 
 
 function pegarLetra(){
     let letra = caixa.value;
     verificarValor(letra);
     caixa.value = "";
 }
+
 let enterclick = (event) =>{
     if(event.key === 'Enter'){
         pegarLetra();
