65f431f688575c9643ef78ffc52819431eaf08b8
alineegal <galvaoaline1102@yahoo.com>
2019-09-21 18:40:55 -0300

    letras

---
+++ jogo-da-forca/script.js
@@ -0,0 +1,36 @@
+let lista = document.querySelectorAll('h1');
+let caixa = document.querySelector('#caixa');
+let botao = document.querySelector('button');
+let chute = document.querySelectorAll('h3');
+
+function verificarValor(letra){
+    let exist = false;
+
+    for  (let h1 of lista){
+        if(letra == h1.innerHTML){
+            h1.style.color = 'white';
+            exist = true;
+        }
+        
+    }
+    
+    if (!exist){
+        alert("erro");
+    }
+}   
+
+function pegarLetra(){
+    let letra = caixa.value;
+    verificarValor(letra);
+    caixa.value = "";
+}
+
+let enterclick = (event) =>{
+    if(event.key === 'Enter'){
+        pegarLetra();
+    }
+}
+
+
+botao.onclick = pegarLetra;
+onkeydown = enterclick;
