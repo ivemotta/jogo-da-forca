511aaf9eeb9ea1a5c40c34ed41a9d1391f465bad
ivemotta <ive_motta@hotmail.com>
2019-09-21 19:56:52 -0300

    criar title e favicon

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let lista = document.querySelectorAll('h1');
 let caixa = document.querySelector('#caixa');
 let botao = document.querySelector('button');
+let chute = document.querySelectorAll('h3');
 
 function verificarValor(letra){
     let exist = false;
@@ -10,12 +11,21 @@ function verificarValor(letra){
             h1.style.color = 'white';
             exist = true;
         }
+        
     }
-    
+
     if (!exist){
         alert("erro");
+        
     }
-} 
+
+    for (let h3 of chute){
+        if(letra == h3.innerHTML){
+            h3.style.color = 'red';
+        }
+
+    }
+}   
 
 function pegarLetra(){
     let letra = caixa.value;
