d3756553494d6c2fa3a5510b7c3dd609097a7d03
ivemotta <ive_motta@hotmail.com>
2019-09-21 23:28:46 -0300

    adicionar function da forca

--- script.js
+++ script.js
@@ -2,6 +2,29 @@ let lista = document.querySelectorAll('h1');
 let caixa = document.querySelector('#caixa');
 let botao = document.querySelector('button');
 let chute = document.querySelectorAll('h3');
+let imagem = document.getElementById("trocarImagem")
+
+function tomara(){
+        if (imagem.src.match("forca0")){
+            imagem.src="images/forca1.jpg"
+        }
+        else if (imagem.src.match("forca1")){
+            imagem.src="images/forca2.jpg"
+        }
+        else if (imagem.src.match("forca2")){
+            imagem.src="images/forca3.jpg"
+        }
+        else if (imagem.src.match("forca3")){
+            imagem.src="images/forca4.jpg"
+        }
+        else if (imagem.src.match("forca4")){
+            imagem.src="images/forca5.jpg"
+        }
+        else if (imagem.src.match("forca5")){
+            imagem.src="images/forca6.jpg"
+        }
+}
+
 
 function verificarValor(letra){
     let exist = false;
@@ -15,17 +38,17 @@ function verificarValor(letra){
     }
 
     if (!exist){
-        alert("errou!")
-        
+        alert("errou!") 
+        tomara()
     }
-
+    
     for (let h3 of chute){
         if(letra == h3.innerHTML){
             h3.style.color = 'red';
         }
 
     }
-}   
+} 
 
 function pegarLetra(){
     let letra = caixa.value;
